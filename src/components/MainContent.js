708e1fb081000558e14b3b6885e0818579bdef53
nitishmayekar <nitishmayekar333@gmail.com>
2023-09-22 22:51:12 -0400

    Component Update

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import HeroVideo from './mainContent/HeroVideo';
-import VideoInfo from './mainContent/VideoInfo';
-import Comments from './mainContent/Comments';
-import NextVideos from './mainContent/NextVideos';
+import HeroVideo from './HeroVideo';
+import VideoInfo from './VideoInfo';
+import Comments from './Comments';
+import NextVideos from './NextVideos';
 
 
 
---
+++ src/components/NextVideos.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NextVideos = ({ videos, current }) => {
+  const renderNextVideo = () => {
+    if (videos) {
+      return videos.map(function (next) {
+        // console.log(next);
+        return (
+          <>
+            {next.id !== current.id ? (
+              <div className="next-videos__video" key={next.id}>
+                <Link
+                  to={`/video/${next.id}`}
+                  className="next-videos__video"
+                  key={next.id}
+                >
+                  <img
+                    className="next-videos__video--image"
+                    src={next.image}
+                    alt="picture stuffs"
+                  />
+                  <div className="next-videos__video__info">
+                    <h4 className="next-videos__video__info--title">
+                      {next.title}
+                    </h4>
+                    <h4 className="next-videos__video__info--channel">
+                      {next.channel}
+                    </h4>
+                  </div>
+                </Link>{' '}
+              </div>
+            ) : (
+              <div className="next-videos__video--hide"></div>
+            )}
+          </>
+        );
+      });
+    }
+  };
+
+  return (
+    <div className="next-videos">
+      <h5 className="next-videos--header">NEXT VIDEOS</h5>
+      {renderNextVideo()}
+    </div>
+  );
+};
+export default NextVideos;
